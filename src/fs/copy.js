7a4f1ceef3f20712e45ba700e4ede627a0b78892
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2026-02-28 11:00:06 +0300

    feat: add create function and copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 const copy = async () => {
   // Write your code here
+  const srcPath = path.join(__dirname, "files");
+  const destPath = path.join(__dirname, "files_copy");
+
+  try {
+    // check if source exists
+    await fs.access(srcPath);
+
+    // check if destination already exists
+    try {
+      await fs.access(destPath);
+      throw new Error("Destination already exists"); // files_copy already exists
+    } catch (err) {
+      // destination does not exist -> continue
+      if (err.code !== "ENOENT") throw err;
+    }
+
+    await fs.cp(srcPath, destPath, { recursive: true });
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
