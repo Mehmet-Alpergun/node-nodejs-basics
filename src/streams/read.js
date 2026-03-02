cb9198a6c7fbdfdb9146a52605720a834d7a4b44
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2026-03-02 11:18:22 +0300

    feat: add read, transform and write functions in the streams folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+
+  const readable = createReadStream(filePath, { encoding: "utf-8" });
+
+  readable.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+
+  const writable = createWriteStream(filePath);
+
+  process.stdin.pipe(writable);
 };
 
 await write();
