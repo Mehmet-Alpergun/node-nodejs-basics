f18cca7dc2ba136b519a8e04519be5e3d02af286
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2026-03-03 09:22:16 +0300

    feat: add main and worker functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-  // Write your code here
+  const cpuCount = os.cpus().length;
+  const results = new Array(cpuCount);
+
+  const workers = [];
+
+  for (let i = 0; i < cpuCount; i++) {
+    const worker = new Worker("./worker.js", {
+      workerData: 10 + i,
+    });
+
+    workers.push(
+      new Promise((resolve) => {
+        worker.on("message", (message) => {
+          results[i] = message;
+          resolve();
+        });
+
+        worker.on("error", () => {
+          results[i] = {
+            status: "error",
+            data: null,
+          };
+          resolve();
+        });
+      }),
+    );
+  }
+
+  await Promise.all(workers);
+
+  console.log(results);
 };
 
 await performCalculations();
